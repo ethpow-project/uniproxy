c9fc9e374eba84c5de8b0adb88a723ac1c5ccc43
xinqiyang <yangxinqi@100intl.co.jp>
2021-02-02 15:28:50 +0900

    fix transfer error

--- contracts/ExchangeBroker.sol
+++ contracts/ExchangeBroker.sol
@@ -9,22 +9,14 @@ import "./intf/IERC20.sol";
 import "./lib/Ownable.sol";
 import "./lib/SafeERC20.sol";
 
-// 合约调用Uniswap进行交易
-// 用户的币 打到 我们合约，我们返回用户
-// need user approve 
 contract ExchangeBroker is Ownable {
 
     using SafeERC20 for IERC20;
     using SafeMath for uint256;
 
-    //weth 
-    // kovan weth 0xd0a1e359811322d97991e03f863a0c30c2cf029c
-    // mainnet weth 0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2
-    address public _WETH_ = 0xd0A1E359811322d97991E03f863a0C30C2cF029C;
-
-    //UNI ROUTER 02
-    address public _UNI_ = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
-
+    IUni public uniswapRouter;
+    address public _WETH_;
+    
     fallback() external payable {
         require(msg.sender == _WETH_, "WE_SAVE_YOUR_TOKEN,FALLBACK");
     }
@@ -33,54 +25,48 @@ contract ExchangeBroker is Ownable {
         require(msg.sender == _WETH_, "WE_SAVE_YOUR_TOKEN,RECEIVE");
     }
 
-    function price(address tokenIn,address tokenOut,uint256 amountIn) external view returns(uint256){
-       address[] memory path = new address[](3);
-       path[0] = tokenIn;
-       path[1] = _WETH_;
-       path[2] = tokenOut;
-       uint[] memory amounts = IUni(_UNI_).getAmountsOut(amountIn,path);
-       return amounts[1];
+    event Swap(address indexed account, address[] indexed path, uint amountIn, uint amountOut);
+
+    constructor(address payable _weth, address payable _uni) public Ownable() {
+        uniswapRouter = IUni(_uni);
+        _WETH_ = _weth;
     }
 
-    //ETH , WETH , WBTC   / USDT 
-    function swapProxy(address token, address tokenb, uint256 value) public payable {
+    function swapProxy(
+        uint amountIn,
+        address[] calldata path
+    ) external {
+        uint256 balance = IERC20(path[0]).balanceOf(msg.sender);
+        require(balance >= amountIn, "not sufficient balance");
 
-        uint256 balance = IERC20(token).balanceOf(msg.sender);
-        require(balance >= value, "not sufficient balance");
-
-        IERC20(token).transferFrom(msg.sender, address(this), value);
+        IERC20(path[0]).approve(address(uniswapRouter), amountIn);
+        IERC20(path[0]).transferFrom(msg.sender, address(this), amountIn);
         
-        uint256 _balance = IERC20(token).balanceOf(address(this));
-        if (_balance >= value) {
-            //do save approve
-            IERC20(token).safeApprove(_UNI_, uint256(0));
-            IERC20(token).safeApprove(_UNI_, value);
-            //require(false, "err2");
-            address[] memory path = new address[](3);
-            //set is not eth  
-            path[0] = token;
-            path[1] = _WETH_;
-            path[2] = tokenb;
+        uint256[] memory amounts = uniswapRouter.swapExactTokensForTokens(
+            amountIn,
+            uint256(0),
+            path,
+            address(this),
+            block.timestamp + 300
+        );
 
-            uint256[] memory amounts = IUni(_UNI_).swapExactTokensForTokens(value, uint256(0), path, address(this), now.add(1800));
-            
-            if (amounts[0] > uint256(0)) {
-                //balancer 扣取 3/1000 
-                // _balance = amounts[0].mul(uint256(997)).div(uint256(1000));
-                IERC20(tokenb).transfer(msg.sender, amounts[0]);
-            }
-        }
-        
+        uint256 toOut = amounts[amounts.length - 1].mul(uint256(997)).div(uint256(1000));
+        IERC20(path[path.length - 1]).safeApprove(address(this), 0);
+        IERC20(path[path.length - 1]).safeApprove(address(this), toOut);
+        IERC20(path[path.length - 1]).safeTransfer(msg.sender, toOut);
+        emit Swap(msg.sender, path, amountIn, toOut);
+    }
+ 
+    function getAmountOut(uint amountIn, address[] memory path) private returns(uint256[] memory){
+        return uniswapRouter.getAmountsOut(amountIn, path);
     }
 
-    //提取币到
     function claimFee(address token) external payable onlyOwner {
         uint256 balance = IERC20(token).balanceOf(address(this));
         if (token == address(_WETH_)) {
-            //IERC20(_WETH_).withdraw(balance);
             msg.sender.transfer(balance);
         } else {
-            IERC20(token).transfer(msg.sender, balance);
+            IERC20(token).safeTransfer(msg.sender, balance);
         }
     }
 
--- migrations/2_init_deploy_v1.js
+++ migrations/2_init_deploy_v1.js
@@ -1,5 +1,7 @@
 const ExchangeBroker = artifacts.require("ExchangeBroker");
 
 module.exports = async (deployer) => {
-  await deployer.deploy(ExchangeBroker);
+  const _WETH_ = "0xd0A1E359811322d97991E03f863a0C30C2cF029C";
+  const _UNI_ = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
+  await deployer.deploy(ExchangeBroker, _WETH_, _UNI_);
 };
